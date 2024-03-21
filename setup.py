6b494217cc3a3a9078cc838ba116a6b2e6b1afbf
Kayla Tokash <kayla.tokash@gmail.com>
2024-03-21 19:12:05 -0400

    Updated the binary maker, I think?

--- setup.py
+++ setup.py
@@ -1,16 +1,44 @@
 from distutils.core import setup
-import py2exe, sys, os
+import py2exe
+import sys
+import os
+import zipfile
 
-# console = ['fw2kmldrag.py']
-sys.argv.append('py2exe')
+"""
+NOTE: I have no idea how py2exe works, so I'm winging it. I /think/ that
+the contents of the zip file are what is available so again, I think this
+will create two binaries: one for the drag and drop, and one for the gui.
+It's untested...
+"""
 
+# Precompile
+ZIP_FILE_PATH="./.packaged_files.zip"
+ZIP_CONTENTS=["fw2kmllib.py"]
+
+# Clean old archive if it exists
+if os.path.isfile(ZIP_FILE_PATH):
+    os.remove(ZIP_FILE_PATH)
+
+# Create the zip file with the packages
+with zipfile.ZipFile(ZIP_FILE_PATH, mode="w") as archive:
+    for file in ZIP_CONTENTS:
+        archive.write(file)
+
+# Build Windows Executable
+sys.argv.append("py2exe")
 setup(
-    options={'py2exe': {'bundle_files': 1}},
+    options={"py2exe": {"bundle_files": 1}},
     windows=[
         {
-            'script': "fw2kmldrag.py",
+            "dest_base": "fw2kwl_dragNdrop",
+            "script": "fw2kmldrag.py",
             "icon_resources": [(1, "icon.ico")]
+        },
+        {
+            "dest_base": "fw2kwl_gui",
+            "script": "gui.py",
+            "icon_resources": [(1, "icon.ico")],
         }
     ],
-    zipfile=None,
+    zipfile=ZIP_FILE_PATH,
 )
